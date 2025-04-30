bed5471bc995f48045c474aeceaef9166ef13fee
Jonah Newton <33105911+jonahjnewton@users.noreply.github.com>
2025-04-30 13:02:45 +1000

    Initial multi-query work

--- dust3r/inference.py
+++ dust3r/inference.py
@@ -41,17 +41,70 @@ def loss_of_one_batch(batch, model, criterion, device, symmetrize_batch=False, u
     if symmetrize_batch:
         view1, view2 = make_batch_symmetric(batch)
 
-    with torch.cuda.amp.autocast(enabled=bool(use_amp)):
+    with torch.amp.autocast("cuda", enabled=bool(use_amp)):
         pred1, pred2 = model(view1, view2)
 
         # loss is supposed to be symmetric
-        with torch.cuda.amp.autocast(enabled=False):
-            loss = criterion(view1, view2, pred1, pred2) if criterion is not None else None
+        loss = None
+        if criterion is not None:
+            with torch.amp.autocast("cuda", enabled=False):
+                loss = criterion(view1, view2, pred1, pred2)
 
     result = dict(view1=view1, view2=view2, pred1=pred1, pred2=pred2, loss=loss)
     return result[ret] if ret else result
 
+class MultiQueryAttention(Attention):
+    r"""
+    https://arxiv.org/pdf/1911.02150.pdf
+    """
+    def __init__(self, word_size: int = 512, embed_dim: int = 64, n_query:int=8) -> None:
+        self.embed_dim = embed_dim
+        self.dim_K = torch.tensor(embed_dim, requires_grad=False)
+        self.n_query = n_query
+        self.proj = torch.nn.Linear(in_features=embed_dim * n_query,
+                              out_features=embed_dim, bias=False)
+        delattr(self, 'query')
+        self.querys = torch.nn.ModuleList([
+            torch.nn.Linear(in_features=word_size, out_features=embed_dim, bias=True)
+            for _ in range(n_query)
+        ])
+        self.key = torch.nn.Linear(in_features=word_size, out_features=embed_dim, bias=True)
+        self.value = torch.nn.Linear(in_features=word_size, out_features=embed_dim, bias=True)
 
+    def self_attention(self, Q: Tensor, K: Tensor, V: Tensor,
+                       mask:Optional[BoolTensor]=None) -> Tensor:
+        """
+        Perform self-attention on the input tensors.
+
+        This is a simple implementation of self-attention that uses the dot product attention mechanism.
+        If you are looking for attention with better performance, please try:
+
+        * `F.scaled_dot_product_attention`
+        * [Flash Attention](https://github.com/Dao-AILab/flash-attention)
+        * [Memory-efficient attention](https://facebookresearch.github.io/xformers/components/ops.html)
+
+        Args:
+            Q (torch.Tensor): The query tensor.
+            K (torch.Tensor): The key tensor.
+            V (torch.Tensor): The value tensor.
+            mask (Optional[torch.BoolTensor]): A mask tensor used to hide specific positions in the input sequence.
+                It should have the same shape as Q, K, and must be a Boolean tensor with 0s indicating positions to be masked.
+                Use `None` for no masking. Default is `None`.
+        Returns:
+            The output tensor of the self-attention layer.
+        """
+        loss_of_one_batch(collate_with_cat(pairs[i:i + batch_size]), model, None, device)
+        return Z
+                           
+    def forward(self, x: Tensor, mask:Optional[BoolTensor]=None) -> Tensor:
+        K = self.key(x)
+        V = self.value(x)
+        Z_s = torch.cat([
+            self.self_attention(query(x), K, V, mask) for query in self.querys
+        ], dim=1)
+        Z = self.proj(Z_s)
+        return Z
+        
 @torch.no_grad()
 def inference(pairs, model, device, batch_size=8, verbose=True):
     if verbose:
